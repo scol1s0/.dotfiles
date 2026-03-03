f439dd0fcfd698ba4ed2d2ddb295515b04e64b34
Love <love.h.andersson@gmail.com>
2026-03-03 19:20:13 +0100

    fixed command

--- config/core/.config/nvim/lua/config/remap.lua
+++ config/core/.config/nvim/lua/config/remap.lua
@@ -18,4 +18,4 @@ vim.keymap.set("v", "<leader>d", '"_d')
 vim.keymap.set("n", "<leader>s", ":%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<left><left><left>")
 
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
-vim.keymap.set("n", "<leader>r", "<cmd>%<CR>")
+vim.keymap.set("n", "<leader>r", "<cmd>!./%<CR>")
